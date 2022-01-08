2195858e3f37d3f86a2cd84285b86a15ee16bbba
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2022-01-08 23:25:54 +0100

    Add team schema

---
+++ schemas/team.js
@@ -0,0 +1,57 @@
+import { HiUsers as icon } from "react-icons/hi";
+
+export default {
+  name: "team",
+  title: "Team",
+  type: "document",
+  icon,
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "position",
+      title: "Position",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "mail",
+      title: "Mail",
+      type: "string",
+    },
+    {
+      name: "website",
+      title: "Website",
+      type: "url",
+      validation: (Rule) =>
+        Rule.uri({
+          scheme: ["http", "https"],
+        }),
+    },
+    {
+      name: "image",
+      title: "Profilbild",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+  preview: {
+    select: {
+      name: "name",
+      image: "image",
+      position: "position",
+    },
+    prepare: ({ name, image, position }) => ({
+      title: name,
+      subtitle: position,
+      media: image,
+    }),
+  },
+};
